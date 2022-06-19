5f3da4ecca6057d8eb99e7e4f9a3f5473ed6ee04
Senu2002 <106390404+Senu2002@users.noreply.github.com>
2022-06-19 05:45:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['94753420144','94753420144','94753420144'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['94753420144'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '947534201444' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Senura' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'SMC-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Social media club' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
-global.region = 'indonesia'
+global.youtube = 'Pending'
+global.ig = 'https://instagram.com/_s__e_nu_w__a_'
+global.mygc = 'https://chat.whatsapp.com/'
+global.myweb = 'Coming Soon'
+global.email = 'senuwaofficial2@gmail.com'
+global.region = 'srilanka'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© SMC-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Moon_Light' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -73,7 +73,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //—————「 Set Prefix 」—————//
 //gausah di apa² in!
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','/','+','X']
 
 //—————「 Set Simbol 」—————//
 //terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
